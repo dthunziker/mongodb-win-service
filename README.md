5119dfcf9f49ac65ec4f1698e9e19f4589e323b5
Derek Hunziker <dthunziker@gmail.com>
2015-02-17 20:04:59 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ps-mongodb-win-service
-Install MongoDB as a Windows Service via PowerShell
+# Install MongoDB as a Windows Service
+
+### Steps
+1. Install MongoDB from http://www.mongodb.org/downloads
+2. Make note of the installation path
+3. Update the script with the installation path
+4. Run the PowerShell script as Administrator
+
+To reverse what this script does, run: 
+> net stop MongoDB
+> sc delete MongoDB
+> rmdir C:\InstallPath\data
+> rmdir C:\InstallPath\log
+> del C:\InstallPath\mongod.conf
+
+Also, remember to clear out the MongoDB install path from your Environment Variables.
+
+A more advanced script that is capable of creating replica sets is available at:
+https://github.com/Sitecore/psmongoserviceinstall
+
---
+++ install-mongodb-win-service.ps1
@@ -0,0 +1,40 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+# Set this to your MongoDB installation path
+$installPath = "C:\MongoDB"
+
+# Ensure that MongoDB is already installed
+if ((Test-Path -path $installPath) -eq $False)
+{
+    write-host "Please install MongoDB to the following location: $installPath"
+	exit
+}
+
+# Create directories
+md "$installPath\log"
+md "$installPath\data"
+md "$installPath\data\db"
+
+# Server configuration
+$configFileContents = @"
+dbpath={0}\data\db
+logpath={0}\log\mongo.log
+smallfiles=true
+noprealloc=true
+"@
+$stream = [System.IO.StreamWriter] $configPath
+$stream.Write($configFileContents, $installPath)
+$stream.close()
+
+# Install as a Windows service
+& $installPath\bin\mongod.exe --config $configPath --install
+
+# Start the service
+& net start mongodb
+
+# Modify system environment variable
+if ($env:Path -like "*MongoDB") {
+    write-host "$installPath is already added to your system's environment variables"
+	exit 
+}
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $dbPath + "\bin", "Machine")
